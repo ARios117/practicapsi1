669dbfc27fd8e058e5ec9b91c30fd24ad1c6f594
ARios117 <alejandro.rios@estudiante.uam.es>
2022-06-15 22:39:15 +0200

    heroku

--- practicaExamen1/settings.py
+++ practicaExamen1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-ALLOWED_HOSTS = ['psi-p3-mario-alex.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['floating-bayou-66982.herokuapp.com', '127.0.0.1']
 
 SECRET_KEY = os.environ.get(
         'DJANGO_SECRET_KEY',

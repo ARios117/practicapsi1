b57cbcdb60d37c1fa54884fb9aefce02c613d3a4
ARios117 <alejandro.rios@estudiante.uam.es>
2022-06-15 23:49:03 +0200

    admin

--- aplicacion/admin.py
+++ aplicacion/admin.py
@@ -1,3 +1,8 @@
+from curses.ascii import US
 from django.contrib import admin
+from aplicacion.models import Usuario, Tweet, Retweet
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Tweet)
+admin.site.register(Retweet)
